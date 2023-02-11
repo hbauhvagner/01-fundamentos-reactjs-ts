75d5e1612d90dc5e2200768ee4dfcce283ac5a81
hbauhvagner <hbauhvagner@gmail.com>
2023-02-10 23:03:42 -0300

    Aprimorando a aplicação - TypeScript no React

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+            />
+            
+            <div className={styles.profile}>
+                <Avatar src='https://github.com/hbauhvagner.png' />
+
+                <strong>Vagner Nascimento</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
